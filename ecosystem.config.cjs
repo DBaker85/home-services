c4a61ee3b6c7ee4f1fbfd03e77670cdd1e3a86f4
David Baker <badcancer22@gmail.com>
2022-11-30 16:34:44 +0100

    more configs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,12 +3,14 @@ module.exports = {
     name: "fan-control",
     script: 'apps/dell-ipmi-fan-control/dist/index.js',
     watch: ['apps/dell-ipmi-fan-control/dist'],
-    instances: 1
+    instances: 1,
+    exec_mode: 'fork'
   }, {
     name: "power-monitor",
     script: 'apps/dell-ipmi-power-monitor/dist/index.js',
     watch: ['apps/dell-ipmi-power-monitor/dist'],
-    instances: 1
+    instances: 1,
+    exec_mode: 'fork'
   }],
 
  
