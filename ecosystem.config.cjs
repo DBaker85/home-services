3492e1ba0a6d1662069a84aba921e6c715cb2a01
David Baker <badcancer22@gmail.com>
2022-12-07 13:38:54 +0100

    change watch

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,12 +3,14 @@ module.exports = {
     name: "fan-control",
     script: 'apps/dell-ipmi-fan-control/dist/index.js',
     instances: 1,
-    exec_mode: 'fork'
+    exec_mode: 'fork',
+    watch:['apps/dell-ipmi-fan-control/dist/index.js']
   }, {
     name: "power-monitor",
     script: 'apps/dell-ipmi-power-monitor/dist/index.js',
     instances: 1,
-    exec_mode: 'fork'
+    exec_mode: 'fork',
+    watch:['apps/dell-ipmi-power-monitor/dist/index.js']
   }],
 
  
