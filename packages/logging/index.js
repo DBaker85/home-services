c93e0c189ec235b9172b38f983c6f0ba9621f10b
David Baker <badcancer22@gmail.com>
2022-12-07 13:46:19 +0100

    fix exports

--- packages/logging/index.js
+++ packages/logging/index.js
@@ -1,8 +1,12 @@
 const { writeFileSync } = require("fs");
 
-export const errorLogger = /*#__PURE__*/ ({ err, appName }) => {
+const errorLogger = /*#__PURE__*/ ({ err, appName }) => {
   const date = new Date();
   const logname = `${appName}_error-log_${date.getDate()}-${date.getMonth()}-${date.getUTCFullYear()}_${date.getHours()}-${date.getMinutes()}-${date.getSeconds()}.log`;
   console.error(err);
   writeFileSync(logname, JSON.stringify(err));
+};
+
+module.exports = {
+  errorLogger,
 };
