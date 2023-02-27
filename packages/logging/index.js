125d03344749cff4548c01f3f1f8e2ac87b51021
David Baker <badcancer22@gmail.com>
2023-02-27 15:31:09 +0100

    update packages and fix logging

--- packages/logging/index.js
+++ packages/logging/index.js
@@ -1,10 +1,11 @@
-const { writeFileSync } = require("fs");
+const { outputFileSync } = require("fs-extra");
+const { join } = require("path");
 
 const errorLogger = /*#__PURE__*/ ({ err, appName }) => {
   const date = new Date();
   const logname = `${appName}_error-log_${date.getDate()}-${date.getMonth()}-${date.getUTCFullYear()}_${date.getHours()}-${date.getMinutes()}-${date.getSeconds()}.log`;
   console.error(err);
-  writeFileSync(logname, JSON.stringify(err));
+  outputFileSync(join("logs", logname), JSON.stringify(err));
 };
 
 module.exports = {
